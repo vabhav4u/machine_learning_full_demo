5f51049fdccaf03c452bf5b322daaad1e5d759db
Your Name <you@example.com>
2026-02-13 13:18:23 +0530

    evaluation

---
+++ notebook/evaluate.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import joblib
+from sklearn.metrics import classification_report
+
+def evaluate_model():
+    df = pd.read_parquet("../feature_store/features.parquet")
+
+    X = df.drop("churn", axis=1)
+    y = df["churn"]
+
+    model = joblib.load("../models/model.pkl")
+
+    predictions = model.predict(X)
+
+    print(classification_report(y, predictions))
+
+if __name__ == "__main__":
+    evaluate_model()
